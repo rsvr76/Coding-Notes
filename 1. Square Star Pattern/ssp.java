c80332627c6e500f794e92fc157b10f8c34922c5
rsvr76 <vishnuramrs07@gmail.com>
2026-07-22 21:16:28 +0530

    First Commit

---
+++ 1. Square Star Pattern/ssp.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class ssp{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+/* 
+Output
+
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+*/
---
+++ 1. Square Star Pattern/ssp.py
@@ -0,0 +1,16 @@
+n = int(input("Enter a Number : "))
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print("*", end = " ")
+    print()
+
+'''
+Output:
+
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+'''
